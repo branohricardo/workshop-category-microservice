d1ec3d74c2ebc10fb661630751e9891584d2434f
Jean-Marie Gueirard <jean-marie.gueirard@ricardo.ch>
2017-03-23 07:58:56 +0100

    Implement basic REST routing for category endpoint
    * Update category struct to private
    * Add error handling for int to string convertion

--- main.go
+++ main.go
@@ -5,22 +5,30 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-type Category struct {
+type category struct {
 	ID   int
 	Name string
 }
 
 func main() {
-	http.HandleFunc("/category", getCategory)
-	http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/category/{ID}", getCategory)
+	http.ListenAndServe(":8080", router)
 }
 
 func getCategory(w http.ResponseWriter, r *http.Request) {
 
-	catID, _ := strconv.Atoi(r.URL.Query().Get("ID"))
-	cat := Category{
+	catID, err := strconv.Atoi(mux.Vars(r)["ID"])
+
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	cat := category{
 		ID:   catID,
 		Name: "iPhone",
 	}
